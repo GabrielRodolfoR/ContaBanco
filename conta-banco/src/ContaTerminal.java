489ba1d5b33ba49cf5028c21f741e3bea2aabc31
GabrielRodolfoR <gabrielrodolforabello@gmail.com>
2025-05-22 18:41:25 -0300

    Projeto de Simulação de uma conta bancária via terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,18 +1,31 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         System.out.println("Bem-vindo ao banco Java!");
 
-        int numero = 1021;
-        String agencia = "067-8";
-        String nomeCliente = "Gabriel Rodolfo";
-        double saldo = 237.48;
+        // Bloco para coleta de Informações do Usuário
+        System.out.println("Para Inciar, digite seu Nome Completo: ");
+        String nomeCliente = scanner.nextLine();
 
-        System.out.println("Para prosseguir, digite o número da Agência: ");
+        System.out.println("Bem Vindo, " + nomeCliente + ". Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
 
+        System.out.println("Digite o número da sua conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Saldo atual: ");
+        double saldo = scanner.nextDouble();
+
+        // Interface de Exbição das informações do cliente:
         System.out.println("====== DADOS DA CONTA ======");
-        System.out.println("Olá, " + nomeCliente + ", obrigado por criar uma conta em nosso banco! Sua agência é: "
-                + agencia + ", conta nº " + numero + ". Seu saldo de: R$" + saldo + " já está disponível para saque!");
+        System.out.println("Olá, " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é: "
+        + agencia + ", conta " + numero + " e seu saldo de R$" + saldo + " já está disponível para saque!");
         System.out.println("============================");
+
+        // Fecha o Scanner
+        scanner.close();
     }
 }
